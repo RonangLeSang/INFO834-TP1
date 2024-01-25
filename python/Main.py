f2287b55b5940699dae97a464058f51bd872bb2f
ronanter <ronan1terras@gmail.com>
2024-01-25 09:33:34 +0100

    [FEATURE] redis linked

--- python/Main.py
+++ python/Main.py
@@ -1,3 +1,25 @@
+import json
+
+import redis
+from datetime import datetime
+
 
 if __name__ == "__main__":
-    print("Hello World")
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+    user1 = {"timeStamp": []}
+    # Get the current timestamp
+    user1["timeStamp"].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    user1["timeStamp"].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    user1["timeStamp"].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    user1["timeStamp"].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    user1["timeStamp"].append(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    user1 = json.dumps(user1)
+
+    r.set('user:1', user1)
+
+    feur = r.get('user:1')
+
+    print(json.loads(feur)["timeStamp"])
+
